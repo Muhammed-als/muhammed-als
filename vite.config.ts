4460e2fe0fbb6be4643f9a3437c3182fa859f9a8
Muhammed Alsabban <muals21@student.sdu.dk>
2024-04-30 00:58:38 +0200

    fix setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/uhammed-als/",
+  base: "/muhammed-als/",
   plugins: [react()],
 })
